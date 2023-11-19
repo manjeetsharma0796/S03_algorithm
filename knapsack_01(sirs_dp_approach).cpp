f8c0965a76784f000cf9c97562e99d035e441de6
manjeetsharma0796 <manjeetsharma0796@gmail.com>
2023-11-19 15:30:48 +0530

    add: knapsack sirs approach dynamic programming

---
+++ knapsack_01(sirs_dp_approach).cpp
@@ -0,0 +1,38 @@
+//This is the same as the sirs algorithm mentioned dynamic programming
+#include <iostream>
+using namespace std;
+
+int p[5] = {0, 1, 2, 5, 6};
+int wt[5] = {0, 2, 3, 4, 5};
+int m = 8, n = 4;
+int v[5][10];
+
+void vnapsacv(int n, int m)
+{
+    for (int i = 0; i <= n; i++)
+    {
+        for (int w = 0; w <= m; w++)
+        {
+            if (i == 0 || w == 0)
+            {
+                v[i][w] = 0;
+            }
+            else if (wt[i] > w)
+            {
+                v[i][w] = v[i - 1][w];
+            }
+            else
+            {
+                v[i][w] = max(v[i - 1][w], p[i] + v[i - 1][w - wt[i]]);
+            }
+        }
+    }
+    cout << v[n][m];
+}
+
+int main()
+{
+
+    vnapsacv(n, m);
+    return 0;
+}
